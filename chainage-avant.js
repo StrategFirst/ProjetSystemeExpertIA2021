c86c0d07f92547f9545eb8a885e7df4fe591dc71
Mathieu <mathieu.toulon@free.fr>
2021-09-30 13:48:20 +0200

    Ajout test et correction

--- chainage-avant.js
+++ chainage-avant.js
@@ -1,8 +1,8 @@
 /*
 ListeRegle : [ 
 	{ 
-		premisse: [ x, y, z, ... ],
-		conclusion: [ a, b, c, ...]
+		si: [ x, y, z, ... ],
+		alors: [ a, b, c, ...]
 	}, ...
 ]
 BaseFait : [ a, b, z, k ]
@@ -14,32 +14,38 @@ et si négatifs il signifie non fait
 function chainageAvant(ListeRegle, BaseFait, objectif=null) {
 	let auMoinsUneRegle = true;
 	let objectifAtteind = false;
-	while( auMoinsUneRegle && ListeRegle.length>0 ) {
+	let dejaUtilise = ListeRegle.map(k=>false);
+	while( auMoinsUneRegle && dejaUtilise.some(k=>!k) ) {
+		console.log(dejaUtilise);
 		auMoinsUneRegle = false;
-		for(regle of ListeRegle) {
-			let applicable = true;
-			for(let p of regle.premisse) {
-				if(p < 0) {
-					//Si fait présent dans la base, mais doit être absent pour la règle
-					if( BaseFait.indexOf(-p) != -1) { 
-						applicable = false;
-						break;
-					}
-				} else {
-					//Si fait absent mais est requis dans la règle
-					if( BaseFait.indexOf(p) == -1) {
-						applicable = false;
-						break;
+		for(let i in ListeRegle) {
+			if( !dejaUtilise[+i] ) {
+				const regle = ListeRegle[i];
+				let applicable = true;
+				for(let p of regle.si) {
+					if(p < 0) {
+						//Si fait présent dans la base, mais doit être absent pour la règle
+						if( BaseFait.indexOf(-p) != -1) { 
+							applicable = false;
+							break;
+						}
+					} else {
+						//Si fait absent mais est requis dans la règle
+						if( BaseFait.indexOf(p) == -1) {
+							applicable = false;
+							break;
+						}
 					}
 				}
-			}
-			if(applicable) {
-				auMoinsUneRegle = true;
-				for(let c of regle.conclusion ) {
-					BaseFait.push(c);
-					if(c == objectif) {
-						objectifAtteind = true; 
+				if(applicable) {
+					auMoinsUneRegle = true;
+					for(let c of regle.alors ) {
+						BaseFait.push(c);
+						if(c == objectif) {
+							objectifAtteind = true; 
+						}
 					}
+					dejaUtilise[+i] = true;
 				}
 			}
 		}
@@ -50,3 +56,43 @@ function chainageAvant(ListeRegle, BaseFait, objectif=null) {
 	};
 }
 
+console.log(
+	chainageAvant(
+		[
+			{
+				si: [1, 2, 3],
+				alors: [4]
+			},
+			{
+				si: [2, 5],
+				alors: [6]
+			},
+			{
+				si: [7, 4],
+				alors: [6]
+			},
+			{
+				si: [1],
+				alors: [8]
+			},
+			{
+				si: [2],
+				alors: [3]
+			},
+			{
+				si: [6, 8],
+				alors: [9]
+			},
+			{
+				si: [7],
+				alors: [2]
+			},
+			{
+				si: [8, 7],
+				alors: [6]
+			}
+		],
+		[7, 1],
+		9
+	)
+);
