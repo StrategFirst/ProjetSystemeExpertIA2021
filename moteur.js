da91127de679a19d257bc79476c44700fb9366e3
Mathieu <mathieu.toulon@free.fr>
2021-10-29 21:53:57 +0200

    Modification du moteur pour un meilleur affichage et résultat

--- moteur.js
+++ moteur.js
@@ -1,8 +1,11 @@
 // Implémentation de méthode custom pour factorisé le code :
 require('./custom_function.js');
+console.logger = require('./custom_log.js');
 
 // Extrait tous les faits possible à partir d'une liste de règles (au format spécifié dans le README.md )
 const get_listeFait = require('./get_listeFait.js');
+
+const chainageAvant = require('./get_chainageAvant.js');
 
 // Récupération des règles dans le fichier regles.json
 let regles = require('./regles.json');
@@ -15,5 +18,6 @@ listeFait.forEach( fait => connaissance[fait] = null );
 
 // Pour l'instant la base de fait est généré aléatoirement à partir de la liste complète de fait :
 let baseFait = listeFait.filter( () => Math.random() > 0.5 );
+for(let fait of baseFait) connaissance[fait] = true;
 
-console.log( {listeFait , connaissance, baseFait} );
+chainageAvant( connaissance , regles );
