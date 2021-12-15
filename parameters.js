a81aa06887cebe451c37903d328680d9df4924db
Mathieu <mathieu.toulon@free.fr>
2021-12-15 23:21:58 +0100

    Passage de la base de fait en paramètre

--- parameters.js
+++ parameters.js
@@ -4,49 +4,58 @@ class ParameterError extends Error {
 	}
 }
 
-// Création d'un espace pour enregistrer stocker les informations cross fichier
-process.data = {};
+try {
+	// Création d'un espace pour enregistrer stocker les informations cross fichier
+	process.data = {};
 
-// Valeur par défaut :
-process.data.tracemode = 'all';
+	// Valeur par défaut :
+	process.data.tracemode = 'all';
 
-// Parcour d'argv pour controller les valeurs
-for(let i=0 ; i<process.argv.length ; i++) {
-	if( process.argv[i] == '-r' || process.argv[i] == '--regles' ) {
-		process.data.regles = require( process.argv[i+1] );
-		i++;
-	} else
-	if( process.argv[i] == '-t' || process.argv[i] == '--traces' ) {
-		process.data.tracemode = process.argv[i+1] ;
-		i++;
+	// Parcour d'argv pour controller les valeurs
+	for(let i=0 ; i<process.argv.length ; i++) {
+		if( process.argv[i] == '-r' || process.argv[i] == '--regles' ) {
+			process.data.regles = require( process.argv[i+1] );
+			i++;
+		} else
+		if( process.argv[i] == '-t' || process.argv[i] == '--traces' ) {
+			process.data.tracemode = process.argv[i+1] ;
+			i++;
+		} else
+		if( process.argv[i] == '-bf' || process.argv[i] == '--basedefait' ) {
+			process.data.basedefait = require( process.argv[i+1] );
+		}
 	}
-}
 
-// le tracemode doit être parmis :
-if( ! process.data.tracemode.match( /(nothing)|(small)|(all)/ ) ) {
-	throw new ParameterError(`The only value accepted for tracemode are : nothing / small / all , but ${process.data.tracemode} has been given instead`);
-}
+	// le tracemode doit être parmis :
+	if( ! process.data.tracemode.match( /(nothing)|(small)|(all)/ ) ) {
+		throw new ParameterError(`The only value accepted for tracemode are : nothing / small / all , but ${process.data.tracemode} has been given instead`);
+	}
 
-// le format des règles doit êtres celui indiquer dans le README
-if( ! (
-	(process.data.regles instanceof Array) &&
-	( process.data.regles.every( regle => (
-		regle instanceof Object &&
-		regle.si instanceof Array &&
-		regle.si.every( premisse => (
-			premisse.cle != undefined &&
-			premisse.valeur != undefined
-		) ) &&
-		regle.alors instanceof Array &&
-		regle.alors.every( conclusion => (
-			conclusion.cle != undefined &&
-			conclusion.valeur != undefined &&
-			(
-				conclusion.operator == undefined ||
-				conclusion.operator.match( /(==)|(<=)|(>=)|(!=)|(>)|(<)/ )
-			)
-		) )
-	) ) )
-) ) {
-	throw new ParameterError(`The given json file must match the syntax given in the README.md file`);
+	// le format des règles doit êtres celui indiquer dans le README
+	if( ! (
+		(process.data.regles instanceof Array) &&
+		( process.data.regles.every( regle => (
+			regle instanceof Object &&
+			regle.si instanceof Array &&
+			regle.si.every( premisse => (
+				premisse.cle != undefined &&
+				premisse.valeur != undefined
+			) ) &&
+			regle.alors instanceof Array &&
+			regle.alors.every( conclusion => (
+				conclusion.cle != undefined &&
+				conclusion.valeur != undefined &&
+				(
+					conclusion.operator == undefined ||
+					conclusion.operator.match( /(==)|(<=)|(>=)|(!=)|(>)|(<)/ )
+				)
+			) )
+		) ) )
+	) ) {
+		throw new ParameterError(`The given json file must match the syntax given in the README.md file`);
+	}
+} catch( err ) {
+	console.error( err );
+	console.trace( err );
+	process.exit( 2 );
 }
