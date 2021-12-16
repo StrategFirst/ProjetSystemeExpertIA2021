263232560a563fd63049bf87d6b9121deaeec794
Mathieu <mathieu.toulon@free.fr>
2021-12-16 01:00:20 +0100

    new custom function clone : deepCopy

--- custom/function.js
+++ custom/function.js
@@ -3,3 +3,6 @@ Array.prototype.uniq = function () { return [... new Set(this)]; }
 
 // Retourne un booléen en fonction de si l'élément fourni en paramètre est présent dans la liste ne prend pas d'autre paramètre et n'altère pas le tableau
 Array.prototype.contains = function (element) { return this.indexOf(element) !== -1; }
+
+// Retourne une copie exact mais distincte de l'élément de base
+Array.prototype.clone = function () { let x = []; for(let e of this) { if(e.clone) x.push(e.clone()); else x.push(e); } return x; }
